65981f91255e9728289c5057f53fa386bdab4c63
Han Wen Xie <hanw.xie@gmail.com>
2020-10-11 01:12:58 -0400

    Added Games API and basic UI

--- src/components/Games/Games.js
+++ src/components/Games/Games.js
@@ -1,5 +1,6 @@
 import React from "react"
-
+import GameCard from "./GameCard"
+import NoGames from "./NoGames"
 class Games extends React.Component {
     constructor() {
         super()
@@ -11,11 +12,33 @@ class Games extends React.Component {
     }
 
     componentDidMount() {
-
+        let today = new Date();
+        let dd = String(today.getDate()).padStart(2, '0');
+        let mm = String(today.getMonth() + 1).padStart(2, '0');
+        let yyyy = today.getFullYear();
+        today = mm + '-' + dd + '-' + yyyy;
+        const url = "https://www.balldontlie.io/api/v1/games?start_date=" + today + "&end_date=" + today;
+        fetch(url)
+        .then(response => response.json())
+        .then(response => {
+            this.setState({
+                isLoaded: true,
+                games: response.data
+            })
+        })
     }
 
     render() {
         const {isLoaded, games} = this.state;
+        let gamesDisplay;
+        console.log(games.length);
+        console.log(games);
+        if (games.length === 0) {
+            gamesDisplay = <NoGames/>
+        }
+        else {
+            gamesDisplay = games.map(game => (<GameCard key={game.id} {...game}/>))
+        }
 
         if (!isLoaded) {
             return (
@@ -25,6 +48,7 @@ class Games extends React.Component {
         return (
             <div>
                 <h1> HELLO FROM GAMES </h1>
+                {gamesDisplay}
             </div>
         )
     }
---
+++ src/components/Games/NoGames.js
@@ -0,0 +1,12 @@
+import React from "react"
+import "./Games.css"
+
+const NoGames = () => {
+    return (
+        <div className="container">
+            <span className="text">No Games Found</span>
+        </div>
+    )
+}
+
+export default NoGames
